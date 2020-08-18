3827ae59a63a124fe3c088dcd5475b6378284804
bootvue <bootvue@gmail.com>
2020-08-18 17:57:17 +0800

    任务调度
    xxl-job

--- service/src/main/java/com/bootvue/service/scheduler/DemoJob.java
+++ service/src/main/java/com/bootvue/service/scheduler/DemoJob.java
@@ -1,17 +1,19 @@
 package com.bootvue.service.scheduler;
 
 
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.elasticjob.api.ShardingContext;
-import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @Slf4j
-public class DemoJob implements SimpleJob {
+public class DemoJob {
 
-    @Override
-    public void execute(final ShardingContext shardingContext) {
-        log.info("shardingContext: {}", shardingContext.toString());
+    @XxlJob("testJob")
+    public ReturnT<String> test(String param) {
+        log.info("参数: {}", param);
+        return new ReturnT<>(200, String.valueOf(System.currentTimeMillis()));
     }
 }
