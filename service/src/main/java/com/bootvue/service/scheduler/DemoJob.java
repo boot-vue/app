c5db27c4b2163c5eb476673162229845b5b976ea
bootvue <bootvue@gmail.com>
2020-08-21 16:30:17 +0800

    format

--- service/src/main/java/com/bootvue/service/scheduler/DemoJob.java
+++ service/src/main/java/com/bootvue/service/scheduler/DemoJob.java
@@ -1,6 +1,5 @@
 package com.bootvue.service.scheduler;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.elasticjob.api.ShardingContext;
 import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
